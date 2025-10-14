71f9b5239d46de18a9709c49fcd28ae274fb54fd
ye0ngeun <duddms2415@naver.com>
2025-10-14 17:47:30 +0900

    [PGS] 이영은 / 예산 / 10

---
+++ yeongeun/[week2] 그리디/pgs_12982.py
@@ -0,0 +1,13 @@
+# 예산
+
+def solution(d, budget):
+    d.sort()
+    total = 0
+    count = 0
+    for cost in d:
+        if total + cost <= budget:
+            total += cost
+            count += 1
+        else:
+            break
+    return count
