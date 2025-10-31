77af20db75e4a56b43f0abd8363d9148e46f388d
Lee Jong Hyuck <101847527+Jhcki222@users.noreply.github.com>
2025-10-31 09:01:47 +0900

    [BOJ] / 이종혁 / 1026 / 20

---
+++ jonghyuck/[week4]정렬/boj_1026.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+answer = 0
+
+A.sort()
+B.sort(reverse=True)
+for i in range(N):
+    answer += A[i] * B[i]
+
+print(answer)
