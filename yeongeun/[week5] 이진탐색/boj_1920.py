bc2bf026b3d85d0dec8b53185353592b517765e5
ye0ngeun <duddms2415@naver.com>
2025-11-04 17:34:14 +0900

    [ãBOJ] 이영은 / 수 찾기

---
+++ yeongeun/[week5] 이진탐색/boj_1920.py
@@ -0,0 +1,16 @@
+# 수 찾기
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+A = set(map(int, input().split()))
+
+M = int(input())
+nums = list(map(int, input().split()))
+
+for num in nums:
+    if num in A:
+        print(1)
+    else:
+        print(0)
